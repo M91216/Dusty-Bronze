3bc9ec1c491df2f34af76b58f6ce6d131cddc389
Michael Bain <mbain@fullsail.edu>
2013-05-16 13:55:12 -0400

    Bronze app Project 2

---
+++ js/json.js
@@ -0,0 +1,406 @@
+//Michael Bain
+//VFW 1302
+//2/26/13
+//Json data
+
+
+var json = {
+	"sessionInfo1":  {
+		"optgroup":  ["Sampling:", "intro"],
+		"uname":     ["User:", "Mark Gonzales"],
+		"email":     ["Email:", "markgonzales@gmail.com"],
+		"phone":     ["Phone:", "647-364-0987"],
+		"album":     ["Album:", "Bitches Brew"],
+		"song":      ["Song:", "Spanish Key"],
+		"artist":    ["Artist:", "Miles Davis"],
+		"label":     ["Label:", "Columbia"],
+		"year":      ["Year:", "1970"],
+		"date":      ["Date:", "2012-22-4"],
+		"store":     ["Store:", "Blecker Bob's"],
+		"minutes":   ["Minutes:", "5"],
+		"comments":  ["Comments:", "Used the drums."],
+		"imported":  ["Import:", "No"],
+		"radio":     ["Release:", "Reissue"]
+		
+				
+	},
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     
+     },
+     "sessionInfo3": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     
+     },
+     "sessionInfo4": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     		
+     		
+	  },
+	  "sessionInfo5":{
+	  	"optgroup":  ["Sampling:", "intro"],
+	  	"uname":     ["User:", "Mark Gonzales"],
+	  	"email":     ["Email:", "markgonzales@gmail.com"],
+	  	"phone":     ["Phone:", "647-364-0987"],
+	  	"album":     ["Album:", "Bitches Brew"],
+	  	"song":      ["Song:", "Spanish Key"],
+	  	"artist":    ["Artist:", "Miles Davis"],
+	  	"label":     ["Label:", "Columbia"],
+	  	"year":      ["Year:", "1970"],
+	  	"date":      ["Date:", "2012-22-4"],
+	  	"store":     ["Store:", "Blecker Bob's"],
+	  	"minutes":   ["Minutes:", "5"],
+	  	"comments":  ["Comments:", "Used the drums."],
+	  	"imported":  ["Import:", "No"],
+	  	"radio":     ["Release:", "Reissue"]
+
+     
+     
+     },
+     "sessionInfo6": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+      },
+      "sessionInfo7":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     		
+     		
+	  },
+	  "sessionInfo8":{
+	  	"optgroup":  ["Sampling:", "intro"],
+	  	"uname":     ["User:", "Mark Gonzales"],
+	  	"email":     ["Email:", "markgonzales@gmail.com"],
+	  	"phone":     ["Phone:", "647-364-0987"],
+	  	"album":     ["Album:", "Bitches Brew"],
+	  	"song":      ["Song:", "Spanish Key"],
+	  	"artist":    ["Artist:", "Miles Davis"],
+	  	"label":     ["Label:", "Columbia"],
+	  	"year":      ["Year:", "1970"],
+	  	"date":      ["Date:", "2012-22-4"],
+	  	"store":     ["Store:", "Blecker Bob's"],
+	  	"minutes":   ["Minutes:", "5"],
+	  	"comments":  ["Comments:", "Used the drums."],
+	  	"imported":  ["Import:", "No"],
+	  	"radio":     ["Release:", "Reissue"]
+
+	  	
+
+     
+      },
+      "sessionInfo9":{
+       	"optgroup":  ["Sampling:", "intro"],
+       	"uname":     ["User:", "Mark Gonzales"],
+       	"email":     ["Email:", "markgonzales@gmail.com"],
+       	"phone":     ["Phone:", "647-364-0987"],
+       	"album":     ["Album:", "Bitches Brew"],
+       	"song":      ["Song:", "Spanish Key"],
+       	"artist":    ["Artist:", "Miles Davis"],
+       	"label":     ["Label:", "Columbia"],
+       	"year":      ["Year:", "1970"],
+       	"date":      ["Date:", "2012-22-4"],
+       	"store":     ["Store:", "Blecker Bob's"],
+       	"minutes":   ["Minutes:", "5"],
+       	"comments":  ["Comments:", "Used the drums."],
+       	"imported":  ["Import:", "No"],
+       	"radio":     ["Release:", "Reissue"]
+
+
+     
+     },
+     "sessionInfo10":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     
+     
+     },
+     "sessionInfo11":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+     },
+     "sessionInfo12":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+  
+  
+     },
+     "sessionInfo13":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+     },
+     "sessionInfo14":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+
+     },
+     "sessionInfo15":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+     },
+     "sessionInfo16":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+      
+      
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     
+     
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mark Gonzales"],
+     	"email":     ["Email:", "markgonzales@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+     	
+     }
+     
+}
